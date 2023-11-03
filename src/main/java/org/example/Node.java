f64977148df6d9d95f37ffe90df1b4575f76305d
BerkeAnna <berke.anna0803@gmail.com>
2023-11-03 14:10:52 +0100

    Setting the start node and the goal node

---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Node extends JButton implements ActionListener {
+
+    Node parent;
+    int col;
+    int row;
+    int gCost;
+    int hCost;
+    int fCost;
+    boolean start;
+    boolean goal;
+    boolean solid;
+    boolean open;
+    boolean checked;
+
+    public Node(int col, int row){
+        this.col = col;
+        this.row= row;
+
+        setBackground(Color.white);
+        setForeground(Color.black);
+        addActionListener(this);
+    }
+
+    public void setAsStart() {
+        setBackground(Color.blue);
+        setForeground(Color.white);
+        setText("Start");
+        start = true;
+    }
+
+    public void setAsGoal() {
+        setBackground(Color.yellow);
+        setForeground(Color.black);
+        setText("Goal");
+        goal = true;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        setBackground(Color.orange);
+
+    }
+}
